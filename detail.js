3144ef8c9f52a52b0daab5007723d4d428a594e6
SHIVANI GUPTA <97029777+shivvi509@users.noreply.github.com>
2022-04-06 13:30:19 +0530

    Checkpoint 11

--- detail.js
+++ detail.js
@@ -1,1 +1,10 @@
-//
+function myFunction(){
+    var adult= document.getElementById("inputAdult").value;
+    var toDate= document.getElementById("inputToDate").value;
+    var fromDate = document.getElementById("inputFromDate").value;
+    const dateone=new date(d1);
+    const datetwo=new date(d2);
+    const time=Math.abs(datetwo-dateone);
+    const days=Math.ceil(time/(1000*60*60*24));
+    document.getElementById('inputTotal').value=1000*days*adult;
+}
